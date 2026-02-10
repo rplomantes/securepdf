9c48fb18b38709f0ddab121e351379abd36b9496
Roy Plomantes <rplomantes@gmail.com>
2026-02-11 07:16:21 +0800

    fix classes 15

--- lib.php
+++ lib.php
@@ -10,35 +10,68 @@ function securepdf_add_instance($data, $mform) {
     $data->timecreated  = time();
     $data->timemodified = time();
 
+    // Insert the module instance
     $id = $DB->insert_record('securepdf', $data);
 
-    // DO NOT call get_coursemodule_from_instance() here!
-    // Save files will be handled automatically later
+    // Save uploaded PDF to file area
+    $context = context_module::instance($data->coursemodule);
+    if ($mform) {
+        file_save_draft_area_files(
+            $data->pdf,
+            $context->id,
+            'mod_securepdf',
+            'pdf',
+            0,
+            ['subdirs'=>0, 'maxfiles'=>1]
+        );
+    }
+
     return $id;
 }
 
 
-
-/**
- * Update an existing Secure PDF instance
- */
-function securepdf_after_add_instance($data, $mform) {
+function securepdf_update_instance($data, $mform) {
     global $DB;
 
-    $cm = get_coursemodule_from_instance('securepdf', $data->id, $data->course, false, MUST_EXIST);
-    $context = context_module::instance($cm->id);
+    $data->timemodified = time();
+    $data->id = $data->instance;
 
-    file_postupdate_standard_filemanager(
-        $data,
-        'pdf',
-        ['subdirs'=>0, 'maxbytes'=>0, 'accepted_types'=>['.pdf']],
-        $context,
-        'mod_securepdf',
-        'pdf',
-        0
-    );
+    $DB->update_record('securepdf', $data);
+
+    $context = context_module::instance($data->coursemodule);
+
+    if ($mform) {
+        file_save_draft_area_files(
+            $data->pdf,
+            $context->id,
+            'mod_securepdf',
+            'pdf',
+            0,
+            ['subdirs'=>0, 'maxfiles'=>1]
+        );
+    }
+
+    return true;
 }
 
+
+function securepdf_pluginfile($course, $cm, $context, $filearea, array $args, $forcedownload, array $options=array()) {
+    require_login($course, true, $cm);
+
+    if ($filearea !== 'pdf') {
+        return false;
+    }
+
+    $fs = get_file_storage();
+    $filename = array_pop($args);
+    $file = $fs->get_file($context->id, 'mod_securepdf', 'pdf', 0, '/', $filename);
+
+    if (!$file) {
+        return false;
+    }
+
+    send_stored_file($file, 0, 0, false);
+}
 
 
 /**
@@ -51,10 +84,18 @@ function securepdf_delete_instance($id) {
         return false;
     }
 
-    $DB->delete_records('securepdf', ['id' => $securepdf->id]);
+    // Delete associated files
+    $cm = get_coursemodule_from_instance('securepdf', $id, $securepdf->course, false, MUST_EXIST);
+    $context = context_module::instance($cm->id);
+    $fs = get_file_storage();
+    $fs->delete_area_files($context->id, 'mod_securepdf', 'pdf');
+
+    // Delete DB record
+    $DB->delete_records('securepdf', ['id' => $id]);
 
     return true;
 }
+
 
 /**
  * Feature support
