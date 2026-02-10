7dfafe1668654f7287405b9501a525473550fb74
Roy Plomantes <rplomantes@gmail.com>
2026-02-10 22:29:46 +0800

    fix classes 5

--- lang/en/securepdf.php
+++ lang/en/securepdf.php
@@ -6,3 +6,4 @@ $string['securepdf:addinstance'] = 'Add a Secure PDF activity';
 $string['securepdf:view'] = 'View Secure PDF';
 $string['pdf'] = 'PDF file';
 $string['download'] = 'Download PDF';
+$string['modulename_help'] = 'Upload and securely display a PDF.';
--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-
 $plugin->component = 'mod_securepdf';
-$plugin->version = 2026021000;
-$plugin->requires = 2022041900; // Moodle 4.0+
-$plugin->cron = 0;
+$plugin->version   = 2026021000;
+$plugin->requires  = 2022112800; // Moodle 4.1
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.1';
